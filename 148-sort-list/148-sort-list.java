ff28f0392fd945e2b3e82a4644f032112b53c819
Ayush Verma <90471763+ayushv45@users.noreply.github.com>
2022-08-06 13:15:49 +0530

    Time: 18 ms (57.98%), Space: 75.5 MB (66.64%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -10,22 +10,55 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        ListNode curr = head;
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        while(curr!=null)
+        if(head == null || head.next == null)
         {
-            minHeap.add(curr.val);
-            curr = curr.next;
+            return head;
         }
-        ListNode newNode = new ListNode(-1);
-        ListNode newHead = newNode;
-        while(!minHeap.isEmpty())
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = head;
+        while(fast!=null && fast.next!=null)
         {
-            int x = minHeap.poll();
-            ListNode temp = new ListNode(x);
-            newNode.next = temp;
-            newNode = newNode.next;
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return newHead.next;
+        prev.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+        return merge(left,right);
+    }
+    private static ListNode merge(ListNode left,ListNode right)
+    {
+        ListNode tempNode1 =left;
+        ListNode tempNode2 = right;
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+        while(tempNode1!=null && tempNode2!=null)
+        {
+            if(tempNode1.val<=tempNode2.val)
+            {
+                dummy.next = tempNode1;
+                tempNode1 = tempNode1.next;
+            }
+            else{
+                dummy.next = tempNode2;
+                tempNode2 = tempNode2.next;
+            }
+            dummy = dummy.next;
+        }
+        while(tempNode1!=null)
+        {
+            dummy.next = tempNode1;
+            dummy = dummy.next;
+            tempNode1 = tempNode1.next;
+        }
+        while(tempNode2!=null)
+        {
+             dummy.next = tempNode2;
+            dummy = dummy.next;
+            tempNode2 = tempNode2.next;
+        }
+        return head.next;
     }
 }
