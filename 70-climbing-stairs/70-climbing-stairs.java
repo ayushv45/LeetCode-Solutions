7c02792e42af62f0412576dce9358f12a7915509
Ayush Verma <90471763+ayushv45@users.noreply.github.com>
2022-08-11 00:13:42 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (27.11%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return count(n,dp);
+    }
+    int count(int n , int[] dp)
+    {
+        if(n==1 || n==0)
+            return 1;
+        
+        if(dp[n]!=-1)
+            return dp[n];
+        
+        return dp[n] = count(n-1,dp) + count(n-2,dp);
+    }
+}
